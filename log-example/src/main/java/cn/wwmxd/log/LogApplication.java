27e18ad1f55700ed4bd4cb8b3a7622c9770462ee
liwei <qzywyd@163.com>
2019-12-12 16:46:24 +0800

    删除不必要的代码

--- log-example/src/main/java/cn/wwmxd/log/LogApplication.java
+++ log-example/src/main/java/cn/wwmxd/log/LogApplication.java
@@ -1,8 +1,6 @@
 package cn.wwmxd.log;
 
 import cn.wwmxd.EnableOperateLog;
-import cn.wwmxd.util.SpringUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
